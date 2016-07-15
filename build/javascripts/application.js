96c6665d5c2951c86ba4881bf354b46040efd8a8
kurtcunningham <cunningham.kurt@gmail.com>
2016-07-15 10:24:04 -0500

    fixed typo

--- build/javascripts/application.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".modal--trigger").on("change",function(){$(this).is(":checked")?$("body").addClass("modal-open"):$("body").removeClass("modal-open")}),$(".modal-fade-screen, .modal-close").on("click",function(){$(".modal-state:checked").prop("checked",!1).change()}),$(".modal-inner").on("click",function(o){o.stopPropagation()})});
